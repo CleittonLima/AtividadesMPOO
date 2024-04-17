ef55d74041a073080cb9cdbc830f514b989b4ccf
Cleiton0Lima <celittonlima135@gmail.com>
2024-04-17 13:12:12 -0300

    Adição do encapsulamento de Protec no "ID" de todas as classes

--- Sistema de Cadastro.py
+++ Sistema de Cadastro.py
@@ -1,14 +1,19 @@
 class Aluno():
     def __init__(self, id, nome, curso, endereco):
-        self.id = id
+        self._id = id
         self.nome = nome
         self.curso = curso
         self.endereco = endereco
 
+    def get_id(self):
+        return self._id
 class Curso():
     def __init__(self, id, nome):
-        self.id = id
+        self._id = id
         self.nome = nome
+
+    def get_id(self):
+        return self._id
 
 class Endereco():
     def __init__(self, rua, numero, cidade, estado, cep):
@@ -20,16 +25,22 @@ class Endereco():
 
 class Disciplina():
     def __init__(self, id, nome, professor, sala):
-        self.id = id
+        self._id = id
         self.nome = nome
         self.professor = professor
         self.sala = sala
+    
+    def get_id(self):
+        return self._id
 
 class Professor():
     def __init__(self, id, nome):
-        self.id = id
+        self._id = id
         self.nome = nome
 
+    def get_id(self):
+        return self._id
+    
 class Sala():
     def __init__(self, numero, capacidade, disciplina = None):
         self.numero = numero
@@ -39,16 +50,16 @@ class Sala():
 curso1 = Curso(1, "Sistemas de Informação")
 endereco1 = Endereco("Rua Fulano de Tal", 101, "Serra Talhada", "Pernambuco", "12345-678")
 endereco2 = Endereco("Rua ABC", 123, "Triunfo", "Pernambuco", "87654-321")
-professor1 = Professor(1, "Prof. Erisvaldo")
+professor1 = Professor(5, "Prof. Erisvaldo")
 sala1 = Sala(12, 40)
-disciplina1 = Disciplina(1, "MPOO", professor1, sala1)
+disciplina1 = Disciplina(7, "MPOO", professor1, sala1)
 aluno1 = Aluno(1, "Cleiton", curso1, endereco1)
 
 print("=======<> ABA ALUNO <>=======")
-print("Aluno:", aluno1.nome, "| Curso:", curso1.nome, "| Endereço:", aluno1.endereco.rua, ",", aluno1.endereco.cidade, ",", aluno1.endereco.estado)
+print("Aluno:", aluno1.nome, "|", aluno1._id, "|", "| Curso:", curso1.nome, "| Endereço:", aluno1.endereco.rua, ",", aluno1.endereco.cidade, ",", aluno1.endereco.estado)
 print()
 print("=======<> ABA PROFESSOR <>=======")
-print("Professor:", professor1.nome, "| Endereço:", endereco2.rua, ",", endereco2.cidade, ",", endereco2.estado)
+print("Professor:", professor1.nome, "|", professor1._id, "|",  "| Endereço:", endereco2.rua, ",", endereco2.cidade, ",", endereco2.estado)
 print()
 print("=======<> ABA DISCIPLINA <>=======")
-print("Disciplina:", disciplina1.nome, "| Professor", disciplina1.professor.nome, "| Sala:", disciplina1.sala.numero)
+print("Disciplina:", disciplina1.nome,"|", disciplina1._id, "|", "| Professor:", disciplina1.professor.nome, "| Sala:", disciplina1.sala.numero)
